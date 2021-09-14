8f1a3e076aa82a003bead6393b1814259673ed3d
pawelballo@gmail.com <Polak2012>
2021-09-14 11:10:42 +0200

    initial spark commit

---
+++ TaxiDataAnalysis.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+
+if __name__=="__main__":
+    session = SparkSession.builder.appName("TaxiDataAnalysis").getOrCreate()
+    sc = session.sparkContext
+    sc.setLogLevel("ERROR")
+    dataFrameReader = session.read
+    responses = dataFrameReader \
+        .option("header", "true") \
+        .option("inferSchema", value = True) \
+        .csv("s3n://taxi-data-new-york//green_tripdata_2019_05.csv")
+    print("Initial commit")
+    print("== Schema==")
+    responses.printSchema()
