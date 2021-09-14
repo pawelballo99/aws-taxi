7dbc154e5d710de91543fa7616e1787b4b044865
pawelballo@gmail.com <Polak2012>
2021-09-14 14:36:58 +0200

    loading datas

--- TaxiDataAnalysis.py
+++ TaxiDataAnalysis.py
@@ -1,14 +1,27 @@
 from pyspark.sql import SparkSession
+import os
+import sys
 
-if __name__=="__main__":
-    session = SparkSession.builder.appName("TaxiDataAnalysis").getOrCreate()
-    sc = session.sparkContext
-    sc.setLogLevel("ERROR")
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
+if __name__ == "__main__":
+
+    session = SparkSession.builder.appName("taxi data analysis").master("local[*]").getOrCreate()
+
     dataFrameReader = session.read
-    responses = dataFrameReader \
-        .option("header", "true") \
-        .option("inferSchema", value = True) \
-        .csv("s3://taxi-data-new-york/green_tripdata_2019_05.csv")
-    print("Initial commit")
-    print("== Schema==")
-    responses.printSchema()
+
+    green_tripdata_2020_05 = dataFrameReader.option("header", "true").csv("data/green_tripdata_2020-05.csv")
+    green_tripdata_2019_05 = dataFrameReader.option("header", "true").csv("data/green_tripdata_2019-05.csv")
+    yellow_tripdata_2020_05 = dataFrameReader.option("header", "true").csv("data/yellow_tripdata_2020-05.csv")
+    yellow_tripdata_2019_05 = dataFrameReader.option("header", "true").csv("data/yellow_tripdata_2019-05.csv")
+    
+    print("green2020")
+    green_tripdata_2020_05.select("lpep_pickup_datetime").show()
+
+    print("green2019")
+    green_tripdata_2019_05.select("lpep_pickup_datetime").show()
+
+    #all_joined = green_tripdata_2020_05.join(green_tripdata_2019_05, "outer")
+
+    session.stop()
